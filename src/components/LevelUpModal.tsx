46d73c0ee1c81d0774a966886e5a72246ef1fd65
marcio30peres <marcio30peres@gmail.com>
2021-02-26 15:56:54 -0300

    correção de bug ao subir de nível

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -3,12 +3,12 @@ import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/LevelUpModal.module.css'
 
 export function LevelUpModal() {
-    const { closeLevelUpModal } = useContext(ChallengesContext)
+    const { closeLevelUpModal, level } = useContext(ChallengesContext)
 
     return (
         <div className={styles.overlay}>
             <div className={styles.container}>
-                <header>2</header>
+                <header>{level}</header>
                 <strong>Parabéns!</strong>
                 <p>Você alcançou um novo nível!</p>
                 <button type='button' onClick={closeLevelUpModal}>
--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -20,7 +20,7 @@ export const CountdownContext = createContext({} as CountdownContextData)
 
 export function CountdownProvider({ children }: CountdownContextProps) {
     const { startNewChallenge } = useContext(ChallengesContext)
-    const [time, setTime] = useState(25 * 60)
+    const [time, setTime] = useState(.05 * 60)
     const [isActive, setIsActive] = useState(false)
     const [hasFinished, setHasFinished] = useState(false)
 
@@ -34,7 +34,7 @@ export function CountdownProvider({ children }: CountdownContextProps) {
     function resetCountdown() {
         clearTimeout(countDownTimeout)
         setIsActive(false)
-        setTime(25 * 60)
+        setTime(.05 * 60)
         setHasFinished(false)
     }
 
